208fdc5e246c0d04af2d914e10f3c697447149cc
MYoshua64 <MYoshua64@users.noreply.github.com>
2020-12-28 22:34:53 +0200

    GAME

---
+++ Moon Jumper/Assets/RocketController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RocketController : MonoBehaviour
+{
+    [SerializeField] private float force = 10f;
+    [SerializeField] private GameObject winPanel;
+    [SerializeField] private GameObject losePanel;
+    [SerializeField] private Text winVelocityText;
+    [SerializeField] private Text loseVelocityText;
+    private Vector2 inputMovement;
+    private Rigidbody2D rocketRB;
+    private float currentVel;
+
+    void Start()
+    {
+        rocketRB = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float upMovement = Input.GetAxisRaw("Vertical") * force;
+        upMovement = Mathf.Max(0, upMovement);
+
+        float torque = Input.GetAxisRaw("Horizontal");
+
+        inputMovement = new Vector2(torque, upMovement);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rocketRB.AddForce(inputMovement.y * transform.up);
+        rocketRB.AddTorque(-inputMovement.x);
+
+        currentVel = rocketRB.velocity.magnitude;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            if (currentVel <= 2)
+            {
+                //successful landing, display win screen
+                winPanel.gameObject.SetActive(true);
+                winVelocityText.text = "Your velocity was: " + currentVel + "[m/s]";
+            }
+            else
+            {
+                losePanel.gameObject.SetActive(true);
+                loseVelocityText.text = "Your velocity was: " + currentVel + "[m/s]";
+            }
+
+            Time.timeScale = 0;
+        }
+    }
+}
---
+++ Moon Jumper/Assets/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.WSA.Input;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void RestartLevel()
+    {
+        Time.timeScale = 1;
+        int levelIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
